2e8415a9828c5c79fa8475b917691c90853a8286
noortan <norajbree7@gmail.com>
2021-08-13 20:11:01 +0300

    updating

--- src/components/hornedBeasts.js
+++ src/components/hornedBeasts.js
@@ -13,3 +13,20 @@ import React, { Component } from 'react'
 }
 
 export default hornedBeasts
+import React, { Component } from 'react'
+
+ class hornedBeasts extends Component {
+    render() {
+        return (
+            <div>
+                
+               <h2>  {this.props.title}  </h2>
+               <img src={this.props.image} alt={this.props.title}/>
+               <p> {this.props.description}</p>
+               
+            </div>
+        )
+    }
+}
+
+export default hornedBeasts
